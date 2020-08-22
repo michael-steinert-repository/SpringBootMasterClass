567e7c479db68602dc12690b67725dce0c87d34b
miste003 <michael.steinert003@stud.fh-dortmund.de>
2020-08-22 21:00:35 +0200

    initial spring boot

---
+++ src/main/java/com/example/SpringBootMasterClass/SpringBootMasterClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootMasterClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMasterClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMasterClassApplication.class, args);
+	}
+
+}
